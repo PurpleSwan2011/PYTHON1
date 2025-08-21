836d043029160fc1a39d7afa6c4aebd2081387b1
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-08-21 16:55:26 +0530

    f

---
+++ tiime.py
@@ -0,0 +1,13 @@
+def OnSquareTime(n):
+    iteration=0
+    for i in range(0,n):
+        for j in range(0,n):
+            print("*",end="")
+            iteration+=1
+            print("")
+            print("\n n",n,"iterations",iteration,"/n")
+ONSquareTime(5)
+ONSquareTime(4)
+ONSquareTime(3)
+print("\n time")
+print("n")
---
+++ time.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iteration=0
+    print("no",n)
+    iteration+=1
+    print("iterations",iteration,"\n")
+printnumber(10)
+printnumber(20)
+print("\n time")
